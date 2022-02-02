378744396eb8a9df5878a0f1f9d6b548a6052482
Stephen Gutekanst <stephen@sourcegraph.com>
2022-02-02 14:45:50 -0700

    web: welcome: add invite-by-email option to onboarding flow
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- client/web/src/auth/welcome/StartSearching.tsx
+++ client/web/src/auth/welcome/StartSearching.tsx
@@ -1,13 +1,17 @@
 import classNames from 'classnames'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
+import { ErrorAlert } from '@sourcegraph/branded/src/components/alerts'
+import { Form } from '@sourcegraph/branded/src/components/Form'
 import { ErrorLike, isErrorLike } from '@sourcegraph/common'
+import * as GQL from '@sourcegraph/shared/src/schema'
 import { CopyableText } from '@sourcegraph/web/src/components/CopyableText'
 import { PageRoutes } from '@sourcegraph/web/src/routes.constants'
-import { Link, Alert } from '@sourcegraph/wildcard'
+import { Link, Alert, LoadingSpinner, Button } from '@sourcegraph/wildcard'
 
 import { AuthenticatedUser } from '../../auth'
 import { eventLogger } from '../../tracking/eventLogger'
+import { UserAvatar } from '../../user/UserAvatar'
 import { UserExternalServicesOrRepositoriesUpdateProps } from '../../util'
 import { LogoAscii } from '../LogoAscii'
 import { getPostSignUpEvent, RepoSelectionMode } from '../PostSignUpPage'
@@ -75,7 +79,7 @@ export const StartSearching: React.FunctionComponent<StartSearching> = ({
     onError,
     className,
 }) => {
-    const { externalServices, errorServices, loadingServices } = useExternalServices(user.id)
+    const { externalServices, errorServices, loadingServices } = useExternalServices(user.id, true)
     const saveSelectedRepos = useSaveSelectedRepos()
 
     const {
@@ -133,6 +137,73 @@ export const StartSearching: React.FunctionComponent<StartSearching> = ({
         }
     }, [externalServices, saveSelectedRepos, repoSelectionMode, onError, setSelectedSearchContextSpec, user.username])
 
+    const invitableCollaborators = useMemo<GQL.IPerson[]>(() => {
+        const invitable = externalServices ? externalServices.flatMap(host => host.invitableCollaborators) : []
+        eventLogger.log('UserInvitations_DiscoveredCollaborators', {}, invitable.length)
+        return invitable
+    }, [externalServices])
+    const preventSubmit = useCallback((event: React.FormEvent<HTMLFormElement>): void => event.preventDefault(), [])
+    const [query, setQuery] = useState('')
+    const [filteredCollaborators, setFilteredCollaborators] = useState<GQL.IPerson[]>([])
+
+    useMemo(() => {
+        if (query.trim() == '') {
+            setFilteredCollaborators(invitableCollaborators)
+        } else {
+            setFilteredCollaborators(
+                invitableCollaborators.filter(person => person.name.includes(query) || person.email.includes(query))
+            )
+        }
+    }, [query, invitableCollaborators])
+
+    const [inviteError, setInviteError] = useState<ErrorLike | null>(null)
+    const [loadingInvites, setLoadingInvites] = useState<Set<string>>(new Set<string>())
+
+    const invitePerson = useCallback(
+        async (person: GQL.IPerson): Promise<void> => {
+            if (loadingInvites.has(person.email)) {
+                return
+            }
+            setLoadingInvites(new Set(loadingInvites.add(person.email)))
+
+            try {
+                // TODO: actually send GraphQL request to invite via email.
+
+                // dataOrThrowErrors(
+                //     await requestGraphQL<ResendVerificationEmailResult, ResendVerificationEmailVariables>(
+                //         gql`
+                //             mutation ResendVerificationEmail($user: ID!, $email: String!) {
+                //                 resendVerificationEmail(user: $user, email: $email) {
+                //                     alwaysNil
+                //                 }
+                //             }
+                //         `,
+                //         { user, email }
+                //     ).toPromise()
+                // )
+
+                const removed = new Set(loadingInvites)
+                removed.delete(person.email)
+                setLoadingInvites(removed)
+                eventLogger.log('UserInvitations_SentEmailInvite')
+            } catch (error) {
+                setInviteError(error)
+            }
+        },
+        [loadingInvites]
+    )
+    const invitePersonClicked = useCallback(
+        (person: GQL.IPerson) => async (): void => {
+            await invitePerson(person)
+        },
+        [invitePerson]
+    )
+    const inviteAllClicked = useCallback(async (): void => {
+        for (const person of filteredCollaborators) {
+            await invitePerson(person)
+        }
+    }, [invitePerson, filteredCollaborators])
+
     const { showAlert } = useShowAlert(isDoneCloning, fetchError)
     const { currentIndex, setComplete } = useSteps()
 
@@ -149,18 +220,104 @@ export const StartSearching: React.FunctionComponent<StartSearching> = ({
         <div className="m5-5 d-flex">
             <div className={classNames(className, 'mx-2')}>
                 <div className={styles.titleDescription}>
-                    <h3>Introduce your friends to Sourcegraph</h3>
+                    <h3>Introduce friends and colleagues to Sourcegraph</h3>
                     <p className="text-muted mb-4">
-                        Help your friends and co-workers level up with powerful code search.
+                        We’ve selected a few collaborators from your repositories in case you wanted to level them up
+                        with Sourcegraph’s powerful code search.
                     </p>
                 </div>
+                {isErrorLike(inviteError) && <ErrorAlert error={inviteError} />}
                 <div className="border overflow-hidden rounded">
                     <header>
-                        <div className="py-3 px-4 d-flex justify-content-between align-items-center">
-                            <h4 className="m-0">Invite by sending a link</h4>
+                        <div className="py-3 px-3 d-flex justify-content-between align-items-center">
+                            <h4 className="flex-1 m-0">Collaborators</h4>
+                            <Form
+                                onSubmit={preventSubmit}
+                                className="flex-1 d-inline-flex justify-content-between flex-row"
+                            >
+                                <input
+                                    className="form-control"
+                                    type="search"
+                                    placeholder="Filter by email or username"
+                                    name="query"
+                                    autoComplete="off"
+                                    autoCorrect="off"
+                                    autoCapitalize="off"
+                                    spellCheck={false}
+                                    onChange={event => {
+                                        setQuery(event.target.value)
+                                    }}
+                                />
+                            </Form>
                         </div>
                     </header>
-                    <CopyableText className="mb-3 ml-3 mr-3 flex-1" text={inviteURL} flex={true} size={inviteURL.length} />
+                    <div className={classNames('mb-3', styles.invitableCollaborators)}>
+                        {!isLoading &&
+                            filteredCollaborators.map((person, index) => (
+                                <div
+                                    className={classNames(
+                                        'd-flex',
+                                        'ml-3',
+                                        'align-items-center',
+                                        index !== 0 && 'mt-3'
+                                    )}
+                                    key={person.email}
+                                >
+                                    <UserAvatar
+                                        className={classNames('icon-inline', 'mr-3', styles.avatar)}
+                                        user={person}
+                                    />
+                                    <div>
+                                        <strong>{person.displayName}</strong>
+                                        <div className="text-muted">{person.email}</div>
+                                    </div>
+                                    {loadingInvites.has(person.email) ? (
+                                        <LoadingSpinner inline={true} className={classNames('ml-auto', 'mr-3')} />
+                                    ) : (
+                                        <Button
+                                            variant="secondary"
+                                            outline={true}
+                                            size="sm"
+                                            className={classNames('ml-auto', 'mr-3', styles.inviteButton)}
+                                            onClick={invitePersonClicked(person)}
+                                        >
+                                            Invite
+                                        </Button>
+                                    )}
+                                </div>
+                            ))}
+                        {isLoading && (
+                            <div className="text-muted d-flex justify-content-center mt-3">
+                                <LoadingSpinner inline={false} />
+                            </div>
+                        )}
+                        {!isLoading && filteredCollaborators.length === 0 && (
+                            <div className="text-muted d-flex justify-content-center mt-3">
+                                No collaborators found. Try sending them a direct link below
+                            </div>
+                        )}
+                    </div>
+                    <Button
+                        variant="success"
+                        className="d-block ml-auto mb-3 mr-3"
+                        onClick={inviteAllClicked}
+                        disabled={isLoading || filteredCollaborators.length === 0}
+                    >
+                        Invite all
+                    </Button>
+                </div>
+                <div>
+                    <header>
+                        <div className="py-3 d-flex justify-content-between align-items-center">
+                            <h4 className="m-0">Or invite by sending a link</h4>
+                        </div>
+                    </header>
+                    <CopyableText
+                        className="mb-3 ml-3 mr-3 flex-1"
+                        text={inviteURL}
+                        flex={true}
+                        size={inviteURL.length}
+                    />
                 </div>
             </div>
             <div className={classNames(className, 'mx-2')}>
@@ -172,7 +329,7 @@ export const StartSearching: React.FunctionComponent<StartSearching> = ({
                 </div>
                 <div className="border overflow-hidden rounded">
                     <header>
-                        <div className="py-3 px-4 d-flex justify-content-between align-items-center">
+                        <div className="py-4 px-3 d-flex justify-content-between align-items-center">
                             <h4 className="m-0">Activity log</h4>
                             <small className="m-0 text-muted">{statusSummary}</small>
                         </div>
